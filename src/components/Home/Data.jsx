2a04397ce1ed57269359f1ce77844dd904dc5ca7
Vishal <vishaljangid2@outlook.com>
2022-11-30 02:30:39 +0700

    Header comp / home comp - WIP - 1

---
+++ src/components/Home/Data.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Data = () => {
+  return (
+    <div className="home__data">
+        <h1 className="home__title">
+            Vishal Jangid
+        </h1>
+        <h3 className="home__subtitle">Game Developer</h3>
+        <p className="home__description">dadasdasdadasdadadasdadadaadsadadadas</p>
+
+        <a href="" className="button button--flex"> Say Hello SVG</a>
+
+    </div>
+  )
+}
---
+++ src/components/Home/Social.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="" className="home__social-icon" target="_blank">
+            <i className="uil uil-instagram"></i>
+        </a>
+
+        <a href="" className="home__social-icon" target="_blank">
+        <i className="uil uil-instagram"></i>
+            </a>
+
+            <a href="" className="home__social-icon" target="_blank">
+            <i className="uil uil-instagram"></i>
+            </a>
+
+            <a href="" className="home__social-icon" target="_blank">
+            <i className="uil uil-instagram"></i>
+            </a>
+    </div>
+  )
+}
