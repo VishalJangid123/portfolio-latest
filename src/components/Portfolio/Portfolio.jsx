823fd958dd22bfa27d2364cf51d3850afede267a
Vishal <vishaljangid2@outlook.com>
2022-12-08 07:44:54 +0700

    portfolio project slider

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,13 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import "./portfolio.css"
 import Modal from "./Modal";
-
-
 import { onValue, ref } from 'firebase/database';
 import { db } from '../../firebase';
 
 const Portfolio = () => {
-    
 
     const [projects, setProjects] = useState([]);
     const [isloading, setLoading] = useState(true);
@@ -17,6 +14,8 @@ const Portfolio = () => {
 
     const [modal, setModal] = useState({ show: false, data: null });
 
+    const [activeDot, setActiveDot] = useState(0);
+    const [ totalDots,setTotalDots] = useState(0);
     const handleClose = () => {
         setModal({ show: false, data: null });
       };
@@ -36,13 +35,52 @@ const Portfolio = () => {
         });
     }, []);
 
+    const slideLeft = () => {
+        if(activeDot == 0){
+            return;
+        }
+        var slider = document.getElementById("portfolio__holder-id");
+        slider.scrollLeft = slider.scrollLeft - 600;
+        setTotalDots((slider.clientWidth / 600) + 1)
+        setActiveDot(activeDot == 0 ? 0 : activeDot - 1)
+    }
 
+    const slideRight = () => {
+        if(activeDot == totalDots+1){
+            return;
+        }
+        var slider = document.getElementById("portfolio__holder-id");
+        slider.scrollLeft = slider.scrollLeft + 600;
+        setActiveDot(activeDot == totalDots+1 ? activeDot : activeDot + 1)
+    }
 
+    const slidingDot = (index) => {
+        var slider = document.getElementById("portfolio__holder-id");
+        if(index <= activeDot){
+            slider.scrollLeft = slider.scrollLeft - (600 * (index + activeDot));
+        }
+        else{
+            slider.scrollLeft = slider.scrollLeft + (600 * index);
+        }
+        setActiveDot(index)
+    }
+
+    const dotelement = () => {
+        let total = [];
+        for(let index = 0; index <= totalDots + 1; index++){
+            total.push(<div key={index} className="portfolio__slider-dot" onClick={() => slidingDot(index)}>
+            <i className={activeDot == index ? 'bx bxs-circle' : 'bx bx-circle' }></i>
+        </div>)
+        }
+       return total;
+    }
 
   return (
     <section className="about section" id="about">
       <h2 className="section__title">Portfolio</h2>
       <span className="section__subtitle"> Projects I had worked on</span>
+
+     
 
 
       <div className="portfolio__container container">
@@ -78,7 +116,35 @@ const Portfolio = () => {
 
     </div>
     
-<div className='grid container portfolio__holder'>
+
+    <div className="portfolio__main-container">
+    <div className="portfolio__slider-left-icon" onClick={() => slideLeft()}> 
+            <i className="uil uil-arrow-circle-left"></i>
+        </div>
+
+        <div className="portfolio__slider-right-icon" onClick={() => slideRight()}>
+            <i className="uil uil-arrow-circle-right"></i>
+        </div>
+
+        <div className="portfolio__slider-dots">
+            {
+                dotelement()
+            }
+
+        </div>
+
+        <div className='portfolio__swipe-more'>
+            <span>Swipe for more </span><i class='bx bxs-chevrons-right bx-fade-right'></i>
+        </div>
+        
+
+
+<div className='grid container portfolio__holder' id="portfolio__holder-id">
+   
+
+    
+    
+    
     {
         projects != null && projects[activeNav] != null && isloading === false && 
         projects[activeNav].map((project, index) => {
@@ -107,17 +173,12 @@ const Portfolio = () => {
         })
     }
     </div>
-    
+    </div>
    
     
     {modal.show && modal.data && <Modal closeModal={handleClose} data={modal.data} />}
-    
-    
     </div>
-
-
-
-      </section>
+</section>
   )
 }
 
